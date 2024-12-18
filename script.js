cd8dc52b1a80ad34ccf98904b7878e7581630b13
Anshu kumar <40786793+aanshu-kumar@users.noreply.github.com>
2024-12-18 17:03:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let S1=s1.toUpperCase();
+	let S2=s2.toUpperCase();
+
+	let res = s1.indexOf(s2);
+	if(res>=0)
+		return res;
+	else
+		return -1
 }
 
 // Please do not change the code below
